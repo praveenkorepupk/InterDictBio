3ab10b1f2e6c662860a584d07fc25b487f4619bf
praveenkorepupk <praveen.korepu@excelra.com>
2024-08-29 17:31:36 +0000

    added new changes

---
+++ InterDictBioUpdated/global.R
@@ -0,0 +1,8 @@
+# Similarly, if you have a global.R, you can call onStop() from there.
+# ==== global.R ====
+cat("Doing application setup\n")
+onStop(function() {
+  cat("Doing application cleanup\n")
+})
+# ==== end global.R ====
+options(shiny.maxRequestSize = 32*1024^2)
